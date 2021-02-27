1302f62e5c0a4fb1b88fc668c44c253367e68cfd
Catarina Oralová <catarina.oralova@ibrconsulting.cz>
2021-02-27 11:37:28 +0100

    antd

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {Label} from 'antd';
 
 const App = () => {
-  return <h1>Cestovní itinerář!</h1>;
+  return <>
+  <h1>Cestovní itinerář!</h1>
+  <Label></Label> 
+  </>
 };
 render(<App />, document.querySelector('#app'));
